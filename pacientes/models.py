8ad0a4bfe0003febddd008c7dbd50e58ba2d0e6f
Julio <falecomjulioorlando@outlook.com>
2024-10-28 22:40:42 -0300

    Atualizações

--- pacientes/models.py
+++ pacientes/models.py
@@ -4,10 +4,26 @@ class Paciente(models.Model):
     nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=15)
     endereco = models.CharField(max_length=255)
-    data_nascimento = models.DateField()
+    data_de_nascimento = models.DateField(null=True, blank=True)
     cpf = models.CharField(max_length=11, unique=True)
-    sexo = models.CharField(max_length=10, choices=[('M', 'Masculino'), ('F', 'Feminino')])
+    sexo = models.CharField(max_length=10, choices=[('Masc', 'Masculino'), ('Fem', 'Feminino'), ('O', 'Outro')])
     email = models.EmailField()
+    estado_Civil = models.CharField(max_length=255, choices=[('Não Informado', 'Não Informado'), ('Casado(a)', 'Casado(a)'), ('Solteiro(a)', 'Solteiro(a)'), ('Divorciado(a)', 'Divorciado(a)'), ('Viúvo(a)', 'Viúvo(a)')])
+    filhos = models.CharField(max_length=10, choices=[('Sim', 'Sim'), ('Não', 'Não')])
+    filhos_Quantidade = models.CharField(max_length=10)
+    atendimento= models.CharField(max_length=10, choices=[('Sim', 'Sim'), ('Não', 'Não')])
+    atendimento_Tipo_Tempo_Motivo = models.CharField(max_length=500)
+    religião = models.CharField(max_length=20)
+    escolaridade = models.CharField(max_length=255, choices=[('Fundamental', 'Fundamental'), ('Médio', 'Médio'), ('Superior Completo', 'Superior Completo'), ('Superior incompleto', 'Superior incompleto'), ('Pós-Grauação', 'Pós-Graduação'), ('Mestrado', 'Mestrado'), ('Dotourado', 'Dotourado')])
+    trabalha_no_momento = models.CharField(max_length=10, choices=[('Sim', 'Sim'), ('Não', 'Não')])
+    profissão = models.CharField(max_length=50)
+    toma_Algum_Medicamento =models.CharField(max_length=10, choices=[('Sim', 'Sim'), ('Não', 'Não')])
+    qual_Medicamento =  models.CharField(max_length=100)
+    Disponibilidade = models.CharField(max_length=100)
+    rede_de_apoio = models.CharField(max_length=255)
+    contato_de_emergência = models.CharField(max_length=100)
+    motivo_e_objetivo = models.CharField(max_length=500)
+
 
     def __str__(self):
         return self.nome
